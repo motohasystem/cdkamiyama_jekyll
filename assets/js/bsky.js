77b988bfdc478f182456fa263a957a0cd01d54c9
Daisuke Motohashi <ddssk.m@gmail.com>
2024-04-22 20:04:33 +0900

    URLっぽい文字列をリンク化する

--- assets/js/bsky.js
+++ assets/js/bsky.js
@@ -61,14 +61,15 @@
     // nodeのinnerTextを分析して、URLと思われる部分をリンクに変換する
     function insert_link(node) {
         const text = node.innerText;
-        const urlRegex = /(https?:\/\/[^\s]+)/g;
+        // const urlRegex = /(https?:\/\/[^\s]+)/g;
+        const urlRegex = /([\w.-]+\/[\w\/.-]+)/g;
         const urls = text.match(urlRegex);
         if (urls == null) return node;
 
         urls.forEach((url) => {
             const link = document.createElement("a");
-            link.href = url;
-            link.textContent = url;
+            link.href = "https://" + url;
+            link.textContent = "https://" + url;
             node.innerHTML = node.innerHTML.replace(url, link.outerHTML);
         });
 
